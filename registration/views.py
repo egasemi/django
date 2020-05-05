5aac8c3b76cb022bcf1552b3286e0f4f56275731
emiliano.arregui <emiliano.arregui@gmail.com>
2020-05-05 15:43:51 -0300

    lista de perfiles

--- registration/views.py
+++ registration/views.py
@@ -17,7 +17,7 @@ class SignUpView(CreateView):
     def get_form(self, form_class=None):
         form = super(SignUpView, self).get_form()
 
-        form.fields['username'].widget = forms.TextImput(attrs={'class':'form-control mb-2', 'placeholder':'Nombre de Usuario'})
+        form.fields['username'].widget = forms.TextInput(attrs={'class':'form-control mb-2', 'placeholder':'Nombre de Usuario'})
         form.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control mb-2', 'placeholder':'Email'})
         form.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'placeholder':'Contraseña'})
         form.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'placeholder':'Repetir Contraseña'})
